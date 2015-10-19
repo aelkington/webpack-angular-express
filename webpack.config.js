c381825d536b20394c5149044368f4205f27e4c2
Andy Elkington <elkiandr@gmail.com>
2015-10-18 21:50:31 -0600

    cleaned up webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
+// define paths
 var path = require('path');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 var entryPath = path.resolve(__dirname, 'client', 'app', 'app.js');
@@ -11,56 +12,66 @@ var templatePath = path.resolve(__dirname, 'client', 'index.tpl.html');
 var distPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  devtool: 'eval',
-    /*
-  entry: {
-    hotserver: 'webpack-hot-middleware/client?reload=true',
-    app: entryPath,
-    vendors: ['angular','angular-ui-router','normalize.css']
-  },
-  */
-    entry: [
-        'webpack-hot-middleware/client?reload=true',
-        entryPath
+    devtool: 'eval',
+    entry: {
+        // define webpack hot server to push changes automatically
+        hotserver: 'webpack-hot-middleware/client?reload=true',
+
+        // application entry path
+        app: entryPath,
+
+        // put vendor libraries into their own file
+        vendor: ['angular', 'angular-ui-router', 'normalize.css']
+    },
+
+    // output generated bundled files
+    output: {
+        path: distPath,
+        filename: '[name].js',
+        publicPath: '/'
+    },
+
+    plugins: [
+        // automatically inject dependencies for minification
+        new ngAnnotatePlugin({ add: true }),
+
+        // output vendor common chunck
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+
+        new webpack.optimize.OccurenceOrderPlugin(),
+
+        // load index.html template and inject CSS and JS files
+        new HtmlWebpackPlugin({
+            template: templatePath,
+            inject: 'body',
+            filename: 'index.html'
+        }),
+
+        // webpack server for development
+        new webpack.HotModuleReplacementPlugin(),
+
+        // skip the emitting phase
+        new webpack.NoErrorsPlugin()
+
     ],
-  output: {
-    path: distPath,
-    filename: '[name].js',
-    publicPath: '/'
-  },
-  plugins: [
-    new ngAnnotatePlugin({
-      add: true
-    }),
-    new HtmlWebpackPlugin({
-      template: templatePath,
-      inject: 'body',
-      filename: 'index.html'
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ],
-  module: {
-    preLoaders: [
-        // lint js files
-        {test: /\.js$/, exclude: /node_modules/, loader: "jshint-loader"}
-    ],
-    loaders: [
-        // transpile ES6 -> ES5
-        {test: /\.js?$/, exclude: /node_modules/, loader: 'babel'},
 
-        // loader for JSON files
-        {test: /\.json?$/, loader: 'json'},
+    module: {
+        preLoaders: [
+            // lint js files
+            {test: /\.js$/, exclude: /node_modules/, loader: "jshint-loader"}
+        ],
+        loaders: [
+            // transpile ES6 -> ES5
+            {test: /\.js?$/, exclude: /node_modules/, loader: 'babel'},
 
-        // loader for CSS files
-        {test: /\.css$/, loader: 'style!css'},
+            // loader for JSON files
+            {test: /\.json?$/, loader: 'json'},
 
-        // loader for HTML files
-        {test: /\.html$/,loader: 'html-loader'}
-    ]
-  }
+            // loader for CSS files
+            {test: /\.css$/, loader: 'style!css'},
+
+            // loader for HTML files
+            {test: /\.html$/, loader: 'html-loader'}
+        ]
+    }
 };
