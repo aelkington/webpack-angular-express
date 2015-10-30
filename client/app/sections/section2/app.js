7b5962c5f4b395a980afd1834f4197b5c0790bf7
Andy Elkington <elkiandr@gmail.com>
2015-10-30 00:37:52 -0600

    Implemented code splitting for two sections of a site

---
+++ client/app/sections/section2/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import 'jquery';
+import angular from 'angular';
+import 'angular-ui-router';
+import uibs from 'angular-ui-bootstrap';
+import Common from './../../common/common';
+import Components from './section.components.js';
+import AppComponent from './app.component.js';
+import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+let appModule = angular.module('app', [
+  'ui.router',
+  uibs,
+  Common.name,
+  Components.name
+])
+  .directive('app', AppComponent);
+
+/*
+ * As we are using ES6 with Angular 1.x we can't use ng-app directive
+ * to bootstrap the application as modules are loaded asynchronously.
+ * Instead, we need to bootstrap the application manually
+ */
+angular.element(document).ready(()=> {
+  angular.bootstrap(document, [appModule.name], {strictDi: false});
+});
+
+export default appModule;
